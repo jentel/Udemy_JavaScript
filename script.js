34b01c4d52463495759abda991b93ae1fede4b0e
Jennifer Telisczak <jennifert@magenic.com>
2019-01-25 11:13:38 -0600

    -basic variables

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Hello world!!');
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
