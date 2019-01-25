5b2d52b7638f1e4e504560ccfd6bb8907a3cee39
Jennifer Telisczak <jennifert@magenic.com>
2019-01-25 11:22:39 -0600

    -Type coercion and variable mutation

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-var firstName = 'John';
-console.log(firstName);
+// var firstName = 'John';
+// console.log(firstName);
 
-var lastName = 'Smith';
+// var lastName = 'Smith';
+// var age = 28;
+
+// var fullAge = true;
+// console.log(fullAge);
+
+// var job;
+// console.log(job);
+
+// job = 'Teacher';
+// console.log(job);
+
+/***********************************
+* variable mutation and type coercion
+*/
+
+// type coercison
+var firstName = "John";
 var age = 28;
 
-var fullAge = true;
-console.log(fullAge);
+console.log(firstName + " " + age);
 
-var job;
-console.log(job);
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
 
-job = 'Teacher';
-console.log(job);
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
 
+// variable mutation
+age = 'twenty eight';
+job = "driver";
+alert(firstName  + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName);
