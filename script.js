e6ff8d06d6b118ac4bad7956b6a99c556bd2b9ef
Jennifer Telisczak <jennifert@magenic.com>
2019-02-05 09:25:03 -0600

    -First Class Functions: Passing Functions as Arguments

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ var jane = Object.create(personProto,
 */
 
 /******************************************************************************/
-// Primatives vs Objects
+/*// Primatives vs Objects
 
 // primatives
 var a = 23;
@@ -86,3 +86,42 @@ function change(a, b) {
 }
 
 change(age, obj); // changes obj's references so changes values
+*/
+
+/******************************************************************************/
+// Passing Functions as Arguments
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for(var i = 0; i < arr.length; i++){
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2018 - el;
+}
+
+function isFullAge(el) {
+    return el >= 18;
+}
+
+function maxHeartRate(el) {
+    if(el >= 18 && el <= 81)
+        return Math.round(206.9 - (0.67 * el));
+
+    return -1;
+}
+
+var ages = arrayCalc(years, calculateAge);
+console.log(ages);
+
+var fullAges = arrayCalc(ages, isFullAge);
+console.log(fullAges);
+
+var rates = arrayCalc(ages, maxHeartRate)
+console.log(rates);
+
