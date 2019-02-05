7e22d3aaaf22c6fa544b15a7d49cd81dc7cf27b4
Jennifer Telisczak <jennifert@magenic.com>
2019-02-05 11:04:48 -0600

    -First Class Functions: Functions Returning Functions

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ change(age, obj); // changes obj's references so changes values
 */
 
 /******************************************************************************/
-// Passing Functions as Arguments
+/*// Passing Functions as Arguments
 
 var years = [1990, 1965, 1937, 2005, 1998];
 
@@ -124,4 +124,30 @@ console.log(fullAges);
 
 var rates = arrayCalc(ages, maxHeartRate)
 console.log(rates);
+*/
 
+/******************************************************************************/
+// Functions returning Functions
+
+function interviewQuestion(job) { // returns an object
+    if(job === 'designer'){
+        return function(name){
+            console.log(name + ', can you please explain what UX design is?');
+        } 
+    } else if(job === 'teacher') {
+        return function(name) {
+            console.log('What subject do you teach, ' + name + '?');
+        }
+    } else {
+        return function(name) {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+teacherQuestion('John');
+designerQuestion('Jane');
+
+interviewQuestion('teacher')('Mark');
