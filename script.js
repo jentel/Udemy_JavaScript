45fee56f160df6e26ead17e13fad0b2afde9b2e3
Jennifer Telisczak <jennifert@magenic.com>
2019-02-05 14:19:48 -0600

    -Bind, Call, and Apply

--- script.js
+++ script.js
@@ -178,7 +178,7 @@ interviewQuestion('teacher')('Mark');
 */
 
 /******************************************************************************/
-// Closures
+/*// Closures
 
 function retirement(retirementAge) {
     var a = ' years left until retirement.';
@@ -219,3 +219,47 @@ function interviewQuestion(job) {
 interviewQuestion('teacher')('John');
 interviewQuestion('designer')('Jane');
 interviewQuestion('police officer')('Mark');
+*/
+
+/******************************************************************************/
+// Bind, call, and apply
+
+var john = {
+    name: 'John',
+    age: 26,
+    job: 'teacher',
+    presentation: function(style, timeOfDay) {
+        if(style === 'formal') {
+            console.log('Good ' + 
+                        timeOfDay + ', Ladies and gentlemen! I\'m ' + 
+                        this.name + ', I\'m a ' + 
+                        this.job + ' and I\'m ' + 
+                        this.age + ' years old.');
+        } else if (style === 'friendly') {
+            console.log('Hey! What\'s up? I\'m ' + 
+                        this.name + ', I\'m a ' + 
+                        this.job + ' and I\'m ' + 
+                        this.age + ' years old. Have a nice ' +
+                        timeOfDay + '.'); 
+        }
+    }
+}
+
+john.presentation('formal', 'morning');
+
+var emily = {
+    name: 'Emily',
+    age: 35,
+    job: 'designer'
+}
+
+john.presentation.call(emily, 'friendly', 'afternoon'); // method borrowing
+
+//john.presentation.apply(emily, ['friendly', 'afternoon']); // not work cause not expecting array
+
+var johnFriendly = john.presentation.bind(john, 'friendly'); // bind creates copy of presentation, and returns the function - called carrying
+johnFriendly('morning');
+johnFriendly('night');
+
+var emilyFormal = john.presentation.bind(emily, 'formal');
+emilyFormal('afternoon');
