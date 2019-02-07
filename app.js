6d446caa27805227437654174662b9f5af657fc3
Jennifer Telisczak <jennifert@magenic.com>
2019-02-07 13:03:01 -0600

    -created an Init function

--- app.js
+++ app.js
@@ -35,13 +35,23 @@ var UIController = (function() {
 // Global App Controller
 var controller = (function(budgetCtrl, UICtrl) {
 
-    var DOM = UICtrl.getDOMstrings();
+    var setupEventListeners = function() {
+        var DOM = UICtrl.getDOMstrings();
+        document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function(event) {
+            if(event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
+
+
 
     var ctrlAddItem = function() {
          // Todo:
         // 1. Get the filed input data
         var input = UICtrl.getInput();
-        console.log(input);
 
         // 2. Add the item to the budget controller
 
@@ -50,16 +60,15 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 4. Calculate the budget
 
         // 5. Display the budget on the UI
-    }
+    };
 
-    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function(event) {
-        if(event.keyCode === 13 || event.which === 13) {
-            ctrlAddItem();
+    return {
+        init: function() {
+            console.log('app has started.');
+            setupEventListeners();
         }
-
-
-    });
+    };
 
 })(budgetController, UIController);
+
+controller.init();
