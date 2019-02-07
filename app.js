ced87373d4d2706190761c13a17f10df22f059cb
Jennifer Telisczak <jennifert@magenic.com>
2019-02-07 10:34:35 -0600

    -Reading Input Data

--- app.js
+++ app.js
@@ -7,6 +7,27 @@ var budgetController = (function() { // modular pattern
 // UI controller
 var UIController = (function() {
 
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputButton: '.add__btn'
+    };
+
+    return {
+        getInput: function() {
+
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
 
 })();
 
@@ -14,9 +35,13 @@ var UIController = (function() {
 // Global App Controller
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var DOM = UICtrl.getDOMstrings();
+
     var ctrlAddItem = function() {
          // Todo:
         // 1. Get the filed input data
+        var input = UICtrl.getInput();
+        console.log(input);
 
         // 2. Add the item to the budget controller
 
@@ -25,16 +50,16 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 4. Calculate the budget
 
         // 5. Display the budget on the UI
-
-        console.log('it works');
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event) {
         if(event.keyCode === 13 || event.which === 13) {
             ctrlAddItem();
         }
+
+
     });
 
 })(budgetController, UIController);
