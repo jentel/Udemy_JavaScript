ac81b902148d4f4be1bef385889127598395695e
Jennifer Telisczak <jennifert@magenic.com>
2019-02-07 19:30:02 -0600

    -Adding a new item to the UI

--- app.js
+++ app.js
@@ -64,7 +64,9 @@ var UIController = (function() {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputButton: '.add__btn'
+        inputButton: '.add__btn',
+        incomeContainer: '.income__list',
+        expenseContainer: '.expenses__list'
     };
 
     return {
@@ -75,6 +77,44 @@ var UIController = (function() {
                 description: document.querySelector(DOMstrings.inputDescription).value,
                 value: document.querySelector(DOMstrings.inputValue).value
             };
+        },
+
+        addListItem: function(obj, type) {
+            var htlml, newHtml, element;
+            
+            // create html string with placeholder text
+            if(type === 'inc'){
+                element = DOMstrings.incomeContainer;
+                html = ' <div class="item clearfix" id="income-%id%">' +
+                            '<div class="item__description">%description%</div>' +
+                            '<div class="right clearfix">' +
+                                '<div class="item__value">%value%</div>' +
+                                '<div class="item__delete">' +
+                                    '<button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>' +
+                                '</div>' +
+                            '</div>' +
+                        '</div>';
+            } else if(type === 'exp') {
+                element = DOMstrings.expenseContainer;
+                html =  '<div class="item clearfix" id="expense-%id%">' + 
+                            '<div class="item__description">%description%</div>' +
+                            '<div class="right clearfix">' +
+                                '<div class="item__value">%value%</div>' +
+                                '<div class="item__percentage">21%</div>' +
+                                '<div class="item__delete">' +
+                                    '<button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>' +
+                                '</div>' +
+                            '</div>' +
+                        '</div>';
+            }
+
+            // replace the placeholder text with some actual data
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+
+            // insert the html into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
 
         getDOMstrings: function() {
@@ -111,6 +151,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. Add the item to the UI
+        UICtrl.addListItem(newItem, input.type);
 
         // 4. Calculate the budget
 
